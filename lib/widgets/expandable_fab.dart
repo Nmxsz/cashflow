ab13a08362c80b11405fc309486d9fd29a9f85b2
Christian Plenzdorf <christian.plenzdorf.home+github@gmail.com>
2025-03-28 12:47:45 +0100

    FAB remove animation

--- lib/widgets/expandable_fab.dart
+++ lib/widgets/expandable_fab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 import 'dart:ui';
 
 class ExpandableFab extends StatefulWidget {
@@ -18,42 +17,18 @@ class ExpandableFab extends StatefulWidget {
   State<ExpandableFab> createState() => _ExpandableFabState();
 }
 
-class _ExpandableFabState extends State<ExpandableFab>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _controller;
-  late final Animation<double> _expandAnimation;
+class _ExpandableFabState extends State<ExpandableFab> {
   bool _open = false;
 
   @override
   void initState() {
     super.initState();
     _open = widget.initialOpen ?? false;
-    _controller = AnimationController(
-      value: _open ? 1.0 : 0.0,
-      duration: const Duration(milliseconds: 250),
-      vsync: this,
-    );
-    _expandAnimation = CurvedAnimation(
-      curve: Curves.fastOutSlowIn,
-      reverseCurve: Curves.easeOutQuad,
-      parent: _controller,
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   void _toggle() {
     setState(() {
       _open = !_open;
-      if (_open) {
-        _controller.forward();
-      } else {
-        _controller.reverse();
-      }
     });
   }
 
@@ -92,16 +67,13 @@ class _ExpandableFabState extends State<ExpandableFab>
 
     final children = <Widget>[];
     final count = widget.children.length;
-    const buttonSpacing = 35.0; // Konstanter Abstand zwischen den Buttons
+    const buttonSpacing = 50.0;
 
     for (var i = 0; i < count; i++) {
       children.add(
-        _ExpandingActionButton(
-          index: i,
-          totalCount: count,
-          maxDistance: widget.distance,
-          buttonSpacing: buttonSpacing,
-          progress: _expandAnimation,
+        Positioned(
+          right: 4.0,
+          bottom: 4.0 + 80.0 + (buttonSpacing * i),
           child: widget.children[i],
         ),
       );
@@ -110,94 +82,33 @@ class _ExpandableFabState extends State<ExpandableFab>
   }
 
   Widget _buildToggleButton() {
-    return AnimatedContainer(
-      transformAlignment: Alignment.center,
-      transform: Matrix4.diagonal3Values(
-        _open ? 0.7 : 1.0,
-        _open ? 0.7 : 1.0,
-        1.0,
-      ),
-      duration: const Duration(milliseconds: 250),
-      curve: const Interval(0.0, 0.5, curve: Curves.easeOut),
-      child: SizedBox(
-        width: 56,
-        height: 56,
-        child: Material(
-          shape: const CircleBorder(),
-          clipBehavior: Clip.antiAlias,
-          elevation: 4,
-          child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color(0xFF2196F3), // Blau
-                  Color(0xFF9C27B0), // Lila
-                ],
-              ),
+    return SizedBox(
+      width: 56,
+      height: 56,
+      child: Material(
+        shape: const CircleBorder(),
+        clipBehavior: Clip.antiAlias,
+        elevation: 4,
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF2196F3),
+                Color(0xFF9C27B0),
+              ],
             ),
-            child: InkWell(
-              onTap: _toggle,
-              child: Icon(
-                _open ? Icons.close : Icons.add,
-                size: 32,
-                color: Colors.white,
-              ),
+          ),
+          child: InkWell(
+            onTap: _toggle,
+            child: Icon(
+              _open ? Icons.close : Icons.add,
+              size: 32,
+              color: Colors.white,
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-@immutable
-class _ExpandingActionButton extends StatelessWidget {
-  const _ExpandingActionButton({
-    required this.index,
-    required this.totalCount,
-    required this.maxDistance,
-    required this.buttonSpacing,
-    required this.progress,
-    required this.child,
-  });
-
-  final int index;
-  final int totalCount;
-  final double maxDistance;
-  final double buttonSpacing;
-  final Animation<double> progress;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: progress,
-      builder: (context, child) {
-        // Berechne die vertikale Position mit konstantem Abstand
-        final offset = Offset(
-          0,
-          progress.value *
-              (maxDistance + (buttonSpacing * (totalCount - 1))) *
-              (index / (totalCount - 1)),
-        );
-        return Positioned(
-          right: 4.0 + offset.dx,
-          bottom: 4.0 + offset.dy + 56.0,
-          child: child!,
-        );
-      },
-      child: FadeTransition(
-        opacity: CurvedAnimation(
-          parent: progress,
-          curve: Interval(
-            index / totalCount,
-            (index + 1) / totalCount,
-            curve: Curves.easeOut,
-          ),
-        ),
-        child: child,
       ),
     );
   }
